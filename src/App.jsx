2f4eedc53edeb6240a334355e27e8a48cf321d87
Caroline J. Adesina <caroline.adesina@lbsl.ng>
2025-08-08 23:51:32 +0100

    css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {  Routes, Route } from 'react-router-dom'
-import './App.css'
+import './index.css'
 import './i18n/i18n'
 import Layouts from './components/layouts'
 import Signup from './Pages/Signup'
